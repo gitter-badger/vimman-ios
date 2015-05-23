642906bbc55a799a438d33c3800dee1aef3a248b
tetsuwo <tetsuwo.oishi@gmail.com>
2015-05-23 21:45:39 +0900

    Add gitignore

--- vimman-ios/AppDelegate.m
+++ vimman-ios/AppDelegate.m
@@ -16,6 +16,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSLog(@"Hello, world!!");
+        
     // Override point for customization after application launch.
     return YES;
 }
